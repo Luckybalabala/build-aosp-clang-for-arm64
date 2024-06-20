3f0783513abdcd063c629eecdaa452d7e08e0e79
Luckybalabala <152677283+Luckybalabala@users.noreply.github.com>
2024-06-20 15:23:32 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
+CLANG_PATH=/root/Toolchain/pdx-clang12
+
+export PATH=${CLANG_PATH}/bin:${PATH}
+
+# 设置编译器环境变量为使用 Pdx clang12
+export CC="${CLANG_PATH}/bin/clang"
+export LD="${CLANG_PATH}/bin/ld.lld"
+export CXX="${CLANG_PATH}/bin/clang++"
+
 ./build-llvm.py --vendor-string Pdx \
 --targets ARM AArch64 \
 --projects clang lld \
 --build-stage1-only \
 --no-update \
 -i /root/Toolchain/clang-r498229b
-
 
 #./build-binutils.py --targets arm-linux-gnueabi aarch64-linux-gnu -i /root/Toolchain/pdx-clang16
